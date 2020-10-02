9e77f791ecb9ee02c488e986c6bb653ea9956130
FaraZero <72074720+FaraZero@users.noreply.github.com>
2020-10-02 19:30:39 +0600

    Maze

---
+++ Horror/Assets/Scripts/ColPlay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColPlay : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+            FindObjectOfType<GameManager>().AddScore();
+            Destroy(gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Horror/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public int score = 0;
+    public Text scoreText;
+    public void AddScore()
+    {
+        score += 1;
+        scoreText.text = "coins - "+ score.ToString();
+
+        if (score >= 30)
+        {
+            SceneManager.LoadScene("Win");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
